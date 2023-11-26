3c2cd3fd5c6648f8348cefc1359c8d17b3864216
Diego Auza <auza0002@algonquinlive.com>
2023-11-26 17:14:12 -0500

    adding var at the alignment parameters

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/text_style.dart';
 
+var startAlignment = Alignment.topCenter;
+var endAlignmet = Alignment.bottomCenter;
+
+// this valu con change in the furture, and if I have const at the main container
+// it means those values may change and flutter can not saves them.
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -9,8 +15,8 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+            begin: startAlignment,
+            end: endAlignmet,
             colors: [Colors.blue.shade700, Colors.grey.shade700]),
       ),
       child: const Center(
